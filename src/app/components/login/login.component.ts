00f67dfd2f2a4f74c3cd6d8ef24bdafca7bec84f
gsanders32 <gray.sanders@gmail.com>
2020-07-03 14:03:20 -0500

    login and registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup;
+
+  // get emailInvalidRequired() {
+  //   return this.formLogin.controls.inputEmail.hasError('email') && this.formLogin.controls.email.touched;
+  // }
+  // get emailInvalidMinLength() {
+  //   return this.formLogin.controls.inputEmail.hasError('minLength');
+  // }
+  // get emailInvalidMaxLength() {
+  //   return this.formLogin.controls.inputEmail.hasError('maxLength');
+  // }
+
+  // get passwordInvalidRequired() {
+  //   return this.formLogin.controls.password.hasError('required') && this.formLogin.controls.password.touched;
+  // }
+  // get passwordInvalidMinLength() {
+  //   return this.formLogin.controls.password.hasError('minLength') && this.formLogin.controls.password.touched;
+  // }
+  // get passwordInvalidMaxLength() {
+  //   return this.formLogin.controls.password.hasError('maxLength') && this.formLogin.controls.password.touched;
+  // }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formLogin = this.formBuilder.group({
+      inputEmail: [null, Validators.compose([Validators.required, Validators.email, Validators.minLength(7), Validators.maxLength(100)])],
+      inputPassword: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(100)])]
+    });
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  formRegister: FormGroup;
+
+  get passwordError(): boolean {
+    return this.formRegister.hasError('passwordError');
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formRegister = this.formBuilder.group({
+      inputEmail: [null, Validators.compose([Validators.required, Validators.email, Validators.minLength(7), Validators.maxLength(100)])],
+      inputPassword: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(100)])],
+      inputConfirmPassword: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(100)])]
+    },
+    {
+      validators: this.passwordValidator
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  passwordValidator(control: AbstractControl): ValidationErrors {
+    const password = control.get( 'inputPassword' );
+    const confirmPassword = control.get( 'inputConfirmPassword' );
+    if ( confirmPassword.value !== null && password.value !== confirmPassword.value ) {
+      return { passwordError: true };
+    }
+    return null;
+  }
 }
